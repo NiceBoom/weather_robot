55fd688ce96d5c3c99102d27abcb5ff3550c6fad
NiceBoom <NiceBoomgg@gmail.com>
2021-09-09 20:10:17 +0800

    完善前端页面

--- service/gateway_service/src/main/java/com/robot/GatewayServiceApplication.java
+++ service/gateway_service/src/main/java/com/robot/GatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.robot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +11,11 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+        String gensalt = BCrypt.gensalt();
+        System.out.println(gensalt);
+        String hashpw = BCrypt.hashpw("123456", gensalt);
+        System.out.println("用盐加密后的密码"+hashpw);
+
     }
 
 }
